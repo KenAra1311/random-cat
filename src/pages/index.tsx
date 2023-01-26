2e9ba4f5497b1db5a7a08a98f555ac88bb98055e
KenAra1311 <louis.9814@gmail.com>
2023-01-26 11:35:51 +0900

    importパスの見直し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from 'components/Layout'
+import { useState } from 'react'
+import {randomCatImage as utilRandomCatImage} from 'utils/index'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>ねこ祭り🐱</h1>
-    <img src="https://cdn2.thecatapi.com/images/bpc.jpg" />
-  </Layout>
-)
+const IndexPage = () =>{
+  const [catString, setCatString] = useState<string>("bpc")
+
+  const randomCatImage = () => setCatString(utilRandomCatImage)
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>ねこ祭り🐱</h1>
+      <button onClick={randomCatImage}>ねこを入れ替える🐈</button>
+      <img src={`https://cdn2.thecatapi.com/images/${catString}.jpg`} />
+    </Layout>
+  )
+}
 
 export default IndexPage
--- src/utils/sample-data.ts
+++ src/utils/sample-data.ts
@@ -1,4 +1,5 @@
-import { User } from '../../interfaces'
+import { User } from 'interfaces'
+
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
