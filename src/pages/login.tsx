788038b83843e7cd39feccb313ae6d115d88237d
KenAra1311 <louis.9814@gmail.com>
2023-01-27 09:46:42 +0900

    ログイン画面:認証方法をライブラリに頼らない方式に変更

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,36 @@
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession } from '@supabase/auth-helpers-react'
+import { AuthForm } from 'interfaces/auth_form'
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
 
 const LoginPage: React.FC = () => {
   const router = useRouter()
   const session = useSession()
-  const supabase = useSupabaseClient()
+  const { register, handleSubmit, formState: { errors } } = useForm<AuthForm>()
 
   if (session) router.replace('/')
 
+  const onSubmit = (data: AuthForm) => console.log(data)
+
   return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      localization={{ lang: 'ja' }}
-      theme="dark"
-    />
+    <>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>メールアドレス:</label><br />
+          <input type="text" {...register("email", { required: true })} />
+        </div>
+        {errors.email && <span>メールアドレスは必須です。</span>}
+        <div>
+          <label>パスワード:</label><br />
+          <input type="password" {...register("password", { required: true })} />
+        </div>
+        {errors.password && <span>パスワードは必須です。</span>}
+        <div>
+          <button type="submit">ログイン</button>
+        </div>
+      </form>
+    </>
   )
 }
 
