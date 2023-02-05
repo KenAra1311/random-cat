cc01be2913a997f0475ae9905e8d655a231a7aa2
KenAra1311 <louis.9814@gmail.com>
2023-02-06 08:04:25 +0900

    お気に入り登録の重複無効制約を追加

--- src/repositories/supabase_favorite.ts
+++ src/repositories/supabase_favorite.ts
@@ -1,5 +1,19 @@
-import { SupabaseClient } from '@supabase/supabase-js'
+import { SupabaseClient, User } from '@supabase/supabase-js'
 import { Favorite } from 'interfaces/favorite'
+
+export const fetch = async (
+  supabase: SupabaseClient<any, 'public', any>,
+  { id }: User
+): Promise<Favorite[]> => {
+  const { data, error, status } = await supabase
+    .from('favorites')
+    .select('*')
+    .eq('profile_id', id)
+
+  if (error && status !== 406) throw error
+
+  return data as Favorite[]
+}
 
 export const create = async (
   supabase: SupabaseClient<any, 'public', any>,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { SupabaseClient, User } from '@supabase/supabase-js'
 import { Favorite } from 'interfaces/favorite'
 import { CatImage } from 'interfaces/index'
 import { Dispatch, SetStateAction } from 'react'
-import { create } from 'repositories/supabase_favorite'
+import { create, fetch as fetchFavorites } from 'repositories/supabase_favorite'
 import { v4 as uuidv4 } from 'uuid'
 
 export const fetchCatImage = async (
@@ -21,11 +21,16 @@ export const fetchCatImage = async (
 
 export const createFavorite = async (
   supabase: SupabaseClient<any, 'public', any>,
-  { id }: User,
+  user: User,
   url: string
 ): Promise<void> => {
   try {
-    const favorite: Favorite = { id: uuidv4(), url, profile_id: id }
+    const favorites: Favorite[] = await fetchFavorites(supabase, user)
+    if (favorites.some((f) => f.url === url)) {
+      throw new Error('このねこはすでにお気に入り登録済みです！')
+    }
+
+    const favorite: Favorite = { id: uuidv4(), url, profile_id: user.id }
     await create(supabase, favorite)
 
     alert('表示されてるねこ画像をお気に入りしました！😸')
